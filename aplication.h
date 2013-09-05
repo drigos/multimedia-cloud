2696f69818868a98eeeced9987c1c0763ecf5320
Rodrigo F Martins <martins.drg@gmail.com>
2013-09-05 09:50:37 -0300

    Criado a aplicação to_upper_case()

---
+++ aplication.h
@@ -0,0 +1,12 @@
+#ifndef APLICATION
+#define APLICATION
+
+char to_upper_case(char letter) {
+   if (letter >= 97 && letter <= 122)
+      //'A'=65 e 'a'=97, então 'a'-'A'=32
+      letter -= 32;
+
+   return letter;
+}
+
+#endif

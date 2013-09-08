9462709ba87a84b0edef9c5a0a7928b66cc71b63
Rodrigo F Martins <martins.drg@gmail.com>
2013-09-08 19:24:07 -0300

    Criado níveis de aplicacao

--- application.h
+++ application.h
@@ -1,6 +1,16 @@
 #ifndef APPLICATION
 #define APPLICATION
 
+#define DENSITY1 1
+#define DENSITY2 2
+#define DENSITY3 3
+#define DENSITY4 4
+#define DENSITY5 5
+#define DENSITY6 6
+#define DENSITY7 7
+#define DENSITY8 8
+#define DENSITY9 9
+
 char to_upper_case(char letter) {
    if (letter >= 97 && letter <= 122)
       //'A'=65 e 'a'=97, então 'a'-'A'=32
